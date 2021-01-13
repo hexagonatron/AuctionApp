8a5a180f6d3e9dfa216f0305bfc3b2b6ae25166d
Ben <46476247+hexagonatron@users.noreply.github.com>
2021-01-14 00:17:23 +1030

    Withdrawal functionality implemented.

--- contracts/AuctionContract.sol
+++ contracts/AuctionContract.sol
@@ -4,36 +4,58 @@ pragma solidity >=0.4.21 <0.9.0;
 contract AuctionContract {
     struct AuctionItem {
         uint highestBid;
-        address payable highestBidderAddress;
+        address highestBidderAddress;
+        mapping(address => uint) bids;
         address payable lister;
         string itemName;
         uint auctionEnd;
+        bool claimed;
     }
 
     uint public itemCount;
     mapping(uint => AuctionItem) public auctionItems;
 
     function addListing(string memory _itemName, uint _auctionDuration) public {
-        auctionItems[itemCount] = AuctionItem(
-            0,
-            msg.sender,
-            msg.sender,
-            _itemName,
-            block.timestamp + _auctionDuration
-        );
+        AuctionItem storage item = auctionItems[itemCount];
+        item.highestBid = 0;
+        item.lister = msg.sender;
+        item.itemName = _itemName;
+        item.auctionEnd = block.timestamp + _auctionDuration;
 
         itemCount += 1;
     }
 
     function placeBid(uint _itemId) public payable {
-      require(_itemId >= 0);
-      require(_itemId < itemCount);
-      require(auctionItems[_itemId].auctionEnd > block.timestamp);
-      require(auctionItems[_itemId].highestBid < msg.value);
+      AuctionItem storage item = getAuctionItem(_itemId);
 
-      auctionItems[_itemId].highestBidderAddress.transfer(auctionItems[_itemId].highestBid);
+      require(item.lister != msg.sender, "Can't bid on an item you listed");
+      require(item.auctionEnd > block.timestamp, "Auction has ended");
+      require(item.bids[msg.sender] + msg.value > item.highestBid, "There is a higher bidder");
 
-      auctionItems[_itemId].highestBid = msg.value;
-      auctionItems[_itemId].highestBidderAddress = msg.sender;
+      item.bids[msg.sender] += msg.value;
+      item.highestBid = item.bids[msg.sender];
+      item.highestBidderAddress = msg.sender;
+    }
+
+    function withdraw(uint _itemId) public {
+      AuctionItem storage item = getAuctionItem(_itemId);
+
+      if(item.lister == msg.sender) {
+        require(item.auctionEnd < block.timestamp, "Auction hasn't ended");
+        require(item.claimed == false, "Auction funds already claimed");
+
+        item.claimed = true;
+        msg.sender.transfer(item.highestBid);
+
+      } else {
+        require(item.highestBidderAddress != msg.sender, "Can't withdraw while highest bidder");
+        item.bids[msg.sender] = 0;
+        msg.sender.transfer(item.bids[msg.sender]);
+      }
+    }
+
+    function getAuctionItem(uint _itemId) private view returns (AuctionItem storage) {
+      require(_itemId >= 0 && _itemId < itemCount, "Invalid item Id");
+      return auctionItems[_itemId];
     }
 }
